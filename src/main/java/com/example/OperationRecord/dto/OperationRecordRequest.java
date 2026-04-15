4de6af1cf939bba18922f62aab0efafbdcac7075
入来田雅史 <dghy1795@yahoo.co.jp>
2026-04-15 17:58:47 +0900

    リクエストDTOを作成。

---
+++ src/main/java/com/example/OperationRecord/dto/OperationRecordRequest.java
@@ -0,0 +1,24 @@
+package com.example.OperationRecord.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OperationRecordRequest {
+
+    private Long vehicleId;
+    private Long driverId;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Integer startMeter;
+    private Integer endMeter;
+    private Double fuelRate;
+
+}
