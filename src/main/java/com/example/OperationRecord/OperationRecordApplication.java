19cfef852f9f47a95ca76fcfb3db77531ba9bb54
入来田雅史 <dghy1795@yahoo.co.jp>
2026-04-15 13:13:23 +0900

    プロジェクトの初期化

---
+++ src/main/java/com/example/OperationRecord/OperationRecordApplication.java
@@ -0,0 +1,13 @@
+package com.example.OperationRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OperationRecord/OperationRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OperationRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
