7daf147a25439e7f7c685f33defe1440f0a0b5f1
入来田雅史 <dghy1795@yahoo.co.jp>
2026-04-22 17:17:59 +0900

    ８行目でcontextLoads()テストを無効化。外部依存が多い為。

--- src/test/java/com/example/OperationRecord/OperationRecordApplicationTests.java
+++ src/test/java/com/example/OperationRecord/OperationRecordApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.OperationRecord;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("このアプリは外部設定・DB・LINE Bot に依存するため contextLoads は不要")
 class OperationRecordApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
