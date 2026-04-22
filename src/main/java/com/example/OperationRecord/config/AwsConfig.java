3b1c4077ab0daa2ca8b663c55924844b6d24ed12
入来田雅史 <dghy1795@yahoo.co.jp>
2026-04-22 16:50:10 +0900

    １３行目で、aws_sqs_enabled=trueの時だけ、SqsClient を作るように設定。 １６・１７行目でAWS＿REGIONを環境変数で渡すように設定。 ２３行目のDefaultCredentialsProviderを使って、AWSの認証情報を柔軟に使用できるように設定。

--- src/main/java/com/example/OperationRecord/config/AwsConfig.java
+++ src/main/java/com/example/OperationRecord/config/AwsConfig.java
@@ -1,28 +1,26 @@
 package com.example.OperationRecord.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
+@ConditionalOnProperty(name = "aws.sqs.enabled", havingValue = "true")
 public class AwsConfig {
 
+    @Value("${AWS_REGION}")
+    private String region;
+
     @Bean
-    SqsClient sqsClient() {
+    public SqsClient sqsClient() {
         return SqsClient.builder()
-                .region(Region.of(System.getenv("AWS_REGION")))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(
-                                        System.getenv("AWS_ACCESS_KEY_ID"),
-                                        System.getenv("AWS_SECRET_ACCESS_KEY")
-                                )
-                        )
-                )
-                .build();
+            .region(Region.of(region))
+            .credentialsProvider(DefaultCredentialsProvider.create())
+            .build();
     }
 }
