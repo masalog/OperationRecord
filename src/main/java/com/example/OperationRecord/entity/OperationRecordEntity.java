b4b3aebd96db715b521dc9dbb163a4c2a0430950
入来田雅史 <dghy1795@yahoo.co.jp>
2026-04-25 00:34:00 +0900

    各フィールドに@Columnを使用。DBのスネークケースを、Entityのキャメルケースに変換するという点を明示。

--- src/main/java/com/example/OperationRecord/entity/OperationRecordEntity.java
+++ src/main/java/com/example/OperationRecord/entity/OperationRecordEntity.java
@@ -2,6 +2,7 @@ package com.example.OperationRecord.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,12 +22,25 @@ public class OperationRecordEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long vehicleId;
-    private Long driverId;
-    private LocalDateTime startDateTime;
-    private LocalDateTime endDateTime;
-    private Integer startMeter;
-    private Integer endMeter;
-    private double fuelRate;
-}
 
+    @Column(name = "vehicle_id", nullable = false)
+    private Long vehicleId;
+
+    @Column(name = "driver_id", nullable = false)
+    private Long driverId;
+
+    @Column(name = "start_date_time", nullable = false)
+    private LocalDateTime startDateTime;
+
+    @Column(name = "end_date_time", nullable = false)
+    private LocalDateTime endDateTime;
+
+    @Column(name = "start_meter", nullable = false)
+    private Integer startMeter;
+
+    @Column(name = "end_meter", nullable = false)
+    private Integer endMeter;
+
+    @Column(name = "fuel_rate", nullable = false)
+    private Double fuelRate;
+}
