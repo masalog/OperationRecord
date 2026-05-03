282c614a360271e8a1ada91121804c1b89ab13cb
masalog <masalog@example.com>
2026-05-03 11:50:47 +0900

    （SQLファイル）schema.sqlとdata.sqlを燃費削除に対応。

--- storage/schema.sql
+++ storage/schema.sql
@@ -1,10 +1,11 @@
-CREATE TABLE IF NOT EXISTS operation_records (
+DROP TABLE IF EXISTS operation_records;
+
+CREATE TABLE operation_records (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     vehicle_id BIGINT NOT NULL,
     driver_id BIGINT NOT NULL,
     start_date_time DATETIME NOT NULL,
-    end_date_time DATETIME NOT NULL,
     start_meter INT NOT NULL,
-    end_meter INT NOT NULL,
-    fuel_rate DOUBLE NOT NULL
+    end_date_time DATETIME NULL,
+    end_meter INT NULL
 );
